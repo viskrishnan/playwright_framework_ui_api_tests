942ea400cb7331ac3a8f1d211030a30bc986e6ca
viskrishnan <krishnansubramanian007@gmail.com>
2025-05-21 16:22:40 +0530

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[ ['html'],
    ['junit', { outputFile: 'test-results/e2e-junit-results.xml' }],
+   ['allure-playwright'],
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
